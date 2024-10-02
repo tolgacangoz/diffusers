9eba085fad6163ce190e9dbbc9f83a3f4b3db846
Tolga Cangöz <mtcangoz@gmail.com>
2024-10-02 17:49:10 +0300

    Refactor 'MatryoshkaPipeline' to use mlx tokenizer and handle tokenization parameters

--- examples/community/matryoshka.py
+++ examples/community/matryoshka.py
@@ -94,6 +94,78 @@ EXAMPLE_DOC_STRING = """
         ```
 """
 
+import mlx
+from mlx.data.core import CharTrie
+
+def read_dictionary(token_file):
+    trie_key_scores = []
+    trie = CharTrie()
+
+    f = open(token_file, "rb")
+    sep = "\u2581".encode()
+
+    max_score = 0
+    for line in f:
+        line = line.rstrip()
+        token, score = line.split(b"\t")
+        score = -float(score)
+
+        token = token.replace(sep, b" ")
+        if trie.search(token):
+            raise RuntimeError(b"token " + token + b" already exists")
+        trie.insert(token)
+        trie_key_scores.append(score)
+        max_score = max(max_score, score)
+
+    eos, bos, pad = -1, -1, -1
+    for i in range(trie.num_keys()):
+        key = "".join(trie.key(i))
+        if key == "</s>":
+            eos = i
+        if key == "<unk>":
+            bos = i
+        if key == "<pad>":
+            pad = i
+
+    return trie, trie_key_scores, eos, bos, pad
+
+max_caption_length = 512
+max_token_length = 9
+padding_token = "<pad>"
+class Tokenizer:
+    def __init__(self, token_file):
+        (
+            self._trie,
+            self._trie_key_scores,
+            self.eos,
+            self.bos,
+            self.pad,
+        ) = read_dictionary(token_file)
+        self.vocab_size = self._trie.num_keys()
+
+    @property
+    def trie(self):
+        return self._trie
+
+    @property
+    def trie_key_scores(self):
+        return self._trie_key_scores
+
+    def tokens2text(self, tokens):
+        return "".join([self._trie.key_string(tok) for tok in tokens])
+
+    def token_id(self, token):
+        node = self._trie.search(token)
+        if node is None:
+            raise ValueError(f"token: {token} not found in vocab.")
+        return node.id
+
+tokenizer = Tokenizer("/home/cosmos/Downloads/t5.vocab.txt")
+
+mlx_tokenizer = mlx.data.core.Tokenizer(
+    tokenizer._trie, ignore_unk=True, trie_key_scores=tokenizer.trie_key_scores
+)
+
 
 # Copied from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion.rescale_noise_cfg
 def rescale_noise_cfg(noise_cfg, noise_pred_text, guidance_rescale=0.0):
@@ -3916,77 +3988,6 @@ class MatryoshkaPipeline(
             #     truncation=True,
             #     return_tensors="pt",
             # )
-            import mlx
-            from mlx.data.core import CharTrie
-
-            def read_dictionary(token_file):
-                trie_key_scores = []
-                trie = CharTrie()
-
-                f = open(token_file, "rb")
-                sep = "\u2581".encode()
-
-                max_score = 0
-                for line in f:
-                    line = line.rstrip()
-                    token, score = line.split(b"\t")
-                    score = -float(score)
-
-                    token = token.replace(sep, b" ")
-                    if trie.search(token):
-                        raise RuntimeError(b"token " + token + b" already exists")
-                    trie.insert(token)
-                    trie_key_scores.append(score)
-                    max_score = max(max_score, score)
-
-                eos, bos, pad = -1, -1, -1
-                for i in range(trie.num_keys()):
-                    key = "".join(trie.key(i))
-                    if key == "</s>":
-                        eos = i
-                    if key == "<unk>":
-                        bos = i
-                    if key == "<pad>":
-                        pad = i
-
-                return trie, trie_key_scores, eos, bos, pad
-
-            max_caption_length = 512
-            max_token_length = 128
-            padding_token = "<pad>"
-            class Tokenizer:
-                def __init__(self, token_file):
-                    (
-                        self._trie,
-                        self._trie_key_scores,
-                        self.eos,
-                        self.bos,
-                        self.pad,
-                    ) = read_dictionary(token_file)
-                    self.vocab_size = self._trie.num_keys()
-
-                @property
-                def trie(self):
-                    return self._trie
-
-                @property
-                def trie_key_scores(self):
-                    return self._trie_key_scores
-
-                def tokens2text(self, tokens):
-                    return "".join([self._trie.key_string(tok) for tok in tokens])
-
-                def token_id(self, token):
-                    node = self._trie.search(token)
-                    if node is None:
-                        raise ValueError(f"token: {token} not found in vocab.")
-                    return node.id
-
-            tokenizer = Tokenizer("/home/cosmos/Downloads/t5.vocab.txt")
-
-            mlx_tokenizer = mlx.data.core.Tokenizer(
-                tokenizer._trie, ignore_unk=True, trie_key_scores=tokenizer.trie_key_scores
-            )
 
             d = prompt[: max_caption_length]
             d = " " + d  # Prepad caption with space (or mlx tokenizes wrongly)
