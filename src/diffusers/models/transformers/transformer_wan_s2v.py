132273858de4e97fc50e33b555a69b719ce4ae2e
Tolga Cangöz <mtcangoz@gmail.com>
2025-09-06 12:55:54 +0300

    asd

--- src/diffusers/models/transformers/transformer_wan_s2v.py
+++ src/diffusers/models/transformers/transformer_wan_s2v.py
@@ -629,9 +629,9 @@ class WanS2VRotaryPosEmbed(nn.Module):
 
         # Loop over samples
         output = torch.view_as_complex(
-            torch.empty(
+            torch.zeros(
                 (batch_size, S, self.num_attention_heads,
-                 self.num_attention_heads * self.attention_head_dim // 2,
+                 self.attention_head_dim // 2,
                  2), device=hidden_states.device
                 ).to(torch.float64)
         )
