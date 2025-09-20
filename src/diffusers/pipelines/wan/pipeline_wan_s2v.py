4434c9c24c11ea69a1f483eb3d6e1164d93ddd31
Tolga Cangöz <mtcangoz@gmail.com>
2025-09-20 18:39:47 +0300

    sdf

--- src/diffusers/pipelines/wan/pipeline_wan_s2v.py
+++ src/diffusers/pipelines/wan/pipeline_wan_s2v.py
@@ -875,8 +875,8 @@ class WanSpeechToVideoPipeline(DiffusionPipeline, WanLoraLoaderMixin):
             max_sequence_length=max_sequence_length,
             device=device,
         )
-        diffusers['prompt_embeds'] = prompt_embeds.to("cpu")
-        diffusers['negative_prompt_embeds'] = negative_prompt_embeds.to("cpu")
+        diffusers['prompt_embeds'] = prompt_embeds.detach().clone().to("cpu")
+        diffusers['negative_prompt_embeds'] = negative_prompt_embeds.detach().clone().to("cpu")
         transformer_dtype = self.transformer.dtype
         prompt_embeds = prompt_embeds.to(transformer_dtype)
         if negative_prompt_embeds is not None:
@@ -889,9 +889,9 @@ class WanSpeechToVideoPipeline(DiffusionPipeline, WanLoraLoaderMixin):
         if num_chunks is None or num_chunks > num_chunks_audio:
             num_chunks = num_chunks_audio
         audio_embeds = audio_embeds.to(transformer_dtype)
-        diffusers['audio_input_values'] = asd['input_values'].to("cpu")
-        diffusers['audio_feat'] = asd['feat'].to("cpu")
-        diffusers['audio_embeds'] = audio_embeds.to("cpu")
+        diffusers['audio_input_values'] = asd['input_values'].detach().clone().to("cpu")
+        diffusers['audio_feat'] = asd['feat'].detach().clone().to("cpu")
+        diffusers['audio_embeds'] = audio_embeds.detach().clone().to("cpu")
         latent_motion_frames = (self.motion_frames + 3) // self.vae_scale_factor_temporal
 
         # 5. Prepare latent variables
@@ -933,11 +933,11 @@ class WanSpeechToVideoPipeline(DiffusionPipeline, WanLoraLoaderMixin):
                 latents, condition, videos_last_pixels, motion_latents, pose_condition = latents_outputs
             else:
                 latents = latents_outputs
-            diffusers['latents'] = latents.to("cpu")
-            diffusers['condition'] = condition.to("cpu")
-            diffusers['videos_last_pixels'] = videos_last_pixels.to("cpu")
-            diffusers['motion_latents'] = motion_latents.to("cpu")
-            diffusers['pose_condition'] = pose_condition[r].to("cpu")
+            diffusers['latents'] = latents.detach().clone().to("cpu")
+            diffusers['condition'] = condition.detach().clone().to("cpu")
+            diffusers['videos_last_pixels'] = videos_last_pixels.detach().clone().to("cpu")
+            diffusers['motion_latents'] = motion_latents.detach().clone().to("cpu")
+            diffusers['pose_condition'] = pose_condition[r].detach().clone().to("cpu")
             with torch.no_grad():
                 left_idx = r * num_frames_per_chunk
                 right_idx = r * num_frames_per_chunk + num_frames_per_chunk
@@ -949,7 +949,7 @@ class WanSpeechToVideoPipeline(DiffusionPipeline, WanLoraLoaderMixin):
             # 4. Prepare timesteps by resetting scheduler in each chunk
             self.scheduler.set_timesteps(num_inference_steps, device=device)
             timesteps = self.scheduler.timesteps
-            diffusers['timesteps'] = timesteps.to("cpu")
+            diffusers['timesteps'] = timesteps.detach().clone().to("cpu")
 
             num_warmup_steps = len(timesteps) - num_inference_steps * self.scheduler.order
             self._num_timesteps = len(timesteps)
@@ -979,7 +979,7 @@ class WanSpeechToVideoPipeline(DiffusionPipeline, WanLoraLoaderMixin):
                             attention_kwargs=attention_kwargs,
                             return_dict=False,
                         )[0]
-                    diffusers['noise_pred'] = noise_pred.to("cpu")
+                    diffusers['noise_pred'] = noise_pred.detach().clone().to("cpu")
 
                     if self.do_classifier_free_guidance:
                         with self.transformer.cache_context("uncond"):
@@ -996,11 +996,11 @@ class WanSpeechToVideoPipeline(DiffusionPipeline, WanLoraLoaderMixin):
                                 attention_kwargs=attention_kwargs,
                                 return_dict=False,
                             )[0]
-                            diffusers['noise_uncond'] = noise_uncond.to("cpu")
+                            diffusers['noise_uncond'] = noise_uncond.detach().clone().to("cpu")
                             noise_pred = noise_uncond + guidance_scale * (noise_pred - noise_uncond)
                     # compute the previous noisy sample x_t -> x_t-1
                     latents = self.scheduler.step(noise_pred, t, latents, return_dict=False)[0]
-                    diffusers['scheduler_step'] = latents.to("cpu")
+                    diffusers['scheduler_step'] = latents.detach().clone().to("cpu")
                     if callback_on_step_end is not None:
                         callback_kwargs = {}
                         for k in callback_on_step_end_tensor_inputs:
